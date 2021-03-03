4186a5af23b6beacddfd2c845553edc1088b914a
Eric Hardy <Eric.Hardy@univ-brest.fr>
2021-03-03 18:13:18 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,99 @@
+/*
+multiple OLED displays withnthe same adress :
+Multiple OLED SSD1306 Displays using 2IC : https://forum.arduino.cc/index.php?topic=248663.0
+https://bitbanksoftware.blogspot.com/2019/01/controlling-lots-of-oled-displays-with.html
+TCA9548A I2C Multiplexer : https://www.youtube.com/watch?v=vV42fCpmCFg
+*/
+
+#include <Arduino.h>
+// Include Wire Library for I2C
+#include <Wire.h>
+
+// Include Adafruit Graphics & OLED libraries
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// see https://github.com/adafruit/Adafruit_SSD1306/blob/master/examples/ssd1306_128x32_i2c/ssd1306_128x32_i2c.ino
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET 4
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+int i = 0;
+int d = 0;
+
+void scanI2C() {
+byte error, address; //variable for error and I2C address
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for (address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println("  !");
+      nDevices++;
+    }
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(5000);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("ok, paré...");
+  Wire.begin();
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  //Set the color - always use white despite actual display color
+  display.setTextColor(WHITE);
+  //Set the font size
+  display.setTextSize(2);
+  //Set the cursor coordinates
+  display.setCursor(0,0);
+  display.println("init...");
+  display.display();
+  delay(500);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // scanI2C();
+  display.clearDisplay();
+  display.setCursor(0,0);
+  if (i > 20) {
+    i = 0 ;
+    if (d == 0) {
+      d = 1 ;
+      }
+      else { 
+        d = 0;
+      }
+    display.invertDisplay(d);
+  }
+  //display.print("DroneBot ");
+  display.println(i++);
+  // display.print("Workshop");
+  display.println(i++);
+  display.display();
+  Serial.println(i);
+  delay(500);
+}
